02436e253c88225285a3e6abb657c09e369d45ca
se7enXF <770609835@qq.com>
2020-07-03 14:09:10 +0800

    EzQtTools v1

---
+++ EzQtTools.py
@@ -0,0 +1,157 @@
+# -*- coding:utf-8 -*-
+"""
+@Author: Fei Xue
+@E-Mail: FeiXue@nuaa.edu.cn
+@File: EzQtTools.py
+@Time: 2020/7/3 12:37
+@Introduction: 快捷生成基础窗口和添加布局控件工具
+"""
+
+
+from PySide2 import QtWidgets, QtCore
+__NAME__ = 'EzQtTools'
+
+
+class EzMainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self,
+                 ico=None,
+                 title=__NAME__,
+                 size=(960, 540),
+                 fixed=False,
+                 max_window=False,
+                 show_statusbar=False,
+                 default_layout='V',
+                 default_layout_stretch=0,
+                 default_layout_margins=9,
+                 default_layout_space=9):
+        """
+        直接生成基础的主窗口，并设置默认布局参数
+        :param ico: 窗口图标URL
+        :param title: 窗口标题
+        :param size: 窗口大小（w，h）
+        :param fixed: 窗口大小不可改变？
+        :param max_window: 打开窗口后全屏？
+        :param default_layout: 默认布局，V表示垂直布局，H表示水平布局
+        :param default_layout_stretch: 当前控件在布局中的伸缩量
+        :param default_layout_margins: 布局四周的边界量，可以是整数四元组，也可以是一个整数来表示同值的四元组
+        :param default_layout_space: 布局中控件之间的间隔量
+        """
+        super(EzMainWindow, self).__init__()
+
+        self.default_layout = default_layout
+        self.default_layout_stretch = default_layout_stretch
+        self.default_layout_margins = default_layout_margins
+        self.default_layout_space = default_layout_space
+
+        # -- 初始化默认参数 -- #
+        if ico and QtCore.QFile.exists(ico):
+            self.setWindowIcon(ico)
+        self.setWindowTitle(title)
+        if fixed:
+            self.setFixedSize(size[0], size[1])
+        else:
+            self.resize(size[0], size[1])
+        if max_window:
+            self.showMaximized()
+
+        # -- 初始化中心控件 -- #
+        self.central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        # -- 添加菜单栏和状态栏 -- #
+        # 菜单栏添加菜单后自动显示
+        if show_statusbar:
+            self.statusBar().show()
+
+        # -- 在此函数中设置自己的布局控件 -- #
+        self.__init__widget()
+
+    def add_layout_widget(self, parent, widget, layout=None, stretch=None, margins=None, space=None):
+        """
+        给parent控件上添加widget。当parent首次添加widget，需要指定其布局格式。
+        :param parent: 父控件
+        :param widget: 当前添加的控件
+        :param layout: 布局
+        :param stretch: 伸缩量
+        :param margins: 边界量
+        :param space: 间隔量
+        :return: 当前添加的控件
+        """
+
+        current_layout = parent.layout()
+        if not stretch:
+            stretch = self.default_layout_stretch
+
+        if current_layout:
+            current_layout.addWidget(widget, stretch=stretch)
+        else:
+            if not layout:
+                if self.default_layout == 'H':
+                    layout = QtWidgets.QHBoxLayout()
+                else:
+                    layout = QtWidgets.QVBoxLayout()
+            if not margins:
+                margins = self.default_layout_margins
+            if not space:
+                space = self.default_layout_space
+
+            layout.addWidget(widget, stretch=stretch)
+            layout.setSpacing(space)
+            parent.setLayout(layout)
+
+            if isinstance(margins, int):
+                layout.setContentsMargins(margins, margins, margins, margins)
+            else:
+                layout.setContentsMargins(margins[0], margins[1], margins[2], margins[3])
+
+        return widget
+
+    def status_bar_write(self, words, timeout=0):
+        """
+        在状态栏上显示信息
+        :param keep: 停留时间（毫秒），0表示一致停留直到触发清除或显示信息事件
+        :param words: 信息
+        """
+        self.statusBar().showMessage(words)
+
+    def open_file_dialog(self, default_dir='.', types: list = None):
+        """
+        打开文件或文件夹路径并返回
+        :param default_dir: 打开文件夹对话框默认的目录
+        :param types: 打开文件类型列表，例如 types=['jpg', 'bmp', ...]，当types=None时打开文件夹目录
+        :return: dir
+        """
+        if types is None:
+            src = QtWidgets.QFileDialog.getExistingDirectory(self,
+                                                             '打开目录',
+                                                             default_dir,
+                                                             QtWidgets.QFileDialog.ShowDirsOnly)
+        else:
+            type_str = f'*.{types}'
+            if isinstance(types, list):
+                type_str = f'*.{types[0]}'
+                for t in types[1:]:
+                    type_str += f' *.{t}'
+            src, _ = QtWidgets.QFileDialog.getOpenFileName(self, '打开文件', default_dir, f'File type ({type_str})')
+
+        return src
+
+    def __init__widget(self):
+        """
+        在自定义方法中重写该函数以设置自己的布局；
+        在中心控件上设置布局，首要布局的parent必须指定为self.central_widget；
+
+        例1：只在中心控件上放置标签
+            label = self.add_layout_widget(self.central_widget, QtWidgets.QLabel())
+        例2：在中心空间上放置容器，然后在其中添加两个标签
+            widget = self.add_layout_widget(self.central_widget, QtWidgets.QWidget())
+            label1 = self.add_layout_widget(widget, QtWidgets.QLabel())
+            label2 = self.add_layout_widget(widget, QtWidgets.QLabel())
+        例3：在中心空间上放置两个容器，然后分别在其中添加标签
+            widget1 = self.add_layout_widget(self.central_widget, QtWidgets.QWidget())
+            widget2 = self.add_layout_widget(self.central_widget, QtWidgets.QWidget())
+            label1 = self.add_layout_widget(widget1, QtWidgets.QLabel())
+            label2 = self.add_layout_widget(widget2, QtWidgets.QLabel())
+        """
+        pass
--- Lesson_9.EzQtTools/main.py
+++ Lesson_9.EzQtTools/main.py
@@ -8,108 +8,15 @@
 """
 
 import sys
-import os
-from PySide2 import QtWidgets, QtCore
-from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from EzQtTools import *
+from PySide2.QtWebEngineWidgets import QWebEngineView
 
 
-class EzMainWindow(QtWidgets.QMainWindow):
+class MainWindow(EzMainWindow):
 
-    def __init__(self,
-                 ico=None,
-                 title='浏览器',
-                 size=(960, 540),
-                 fixed=False,
-                 full_screen=False,
-                 default_layout='V',
-                 default_layout_stretch=0,
-                 default_layout_margins=9,
-                 default_layout_space=9):
-        super(EzMainWindow, self).__init__()
-
-        self.default_layout = default_layout
-        self.default_layout_stretch = default_layout_stretch
-        self.default_layout_margins = default_layout_margins
-        self.default_layout_space = default_layout_space
-
-        # -- 初始化默认参数 -- #
-        if ico and os.path.isfile(ico):
-            self.setWindowIcon(ico)
-        self.setWindowTitle(title)
-        if fixed:
-            self.setFixedSize(size[0], size[1])
-        else:
-            self.resize(size[0], size[1])
-        if full_screen:
-            self.setWindowFlag(QtCore.Qt.WindowFullScreen)
-
-        # -- 初始化中心控件 -- #
-        self.central_widget = QtWidgets.QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        # -- 添加菜单栏和状态栏 -- #
-
-        # -- 在此函数中设置自己的布局控件 -- #
+    def __init__(self, **kwargs):
+        EzMainWindow.__init__(self, **kwargs)
         self.__init__widget()
-
-    def add_layout_widget(self, parent, widget, layout=None, stretch=None, margins=None, space=None):
-        """
-        给parent控件上添加widget。当parent首次添加widget，需要指定其布局格式
-        :param parent: 父控件
-        :param widget: 当前添加的控件
-        :param layout: 布局
-        :param stretch: 伸缩量
-        :param margins: 边界量
-        :param space: 间隔量
-        :return: 当前添加的控件
-        """
-
-        current_layout = parent.layout()
-        if not stretch:
-            stretch = self.default_layout_stretch
-
-        if current_layout:
-            current_layout.addWidget(widget, stretch=stretch)
-        else:
-            if not layout:
-                if self.default_layout == 'H':
-                    layout = QtWidgets.QHBoxLayout()
-                else:
-                    layout = QtWidgets.QVBoxLayout()
-            if not margins:
-                margins = self.default_layout_margins
-            if not space:
-                space = self.default_layout_space
-
-            layout.addWidget(widget, stretch=stretch)
-            layout.setSpacing(space)
-            parent.setLayout(layout)
-
-            if isinstance(margins, int):
-                layout.setContentsMargins(margins, margins, margins, margins)
-            else:
-                layout.setContentsMargins(margins[0], margins[1], margins[2], margins[3])
-
-        return widget
-
-    def status_bar_write(self, words):
-        self.statusBar().showMessage(words)
-
-    def open_file_dialog(self, default_dir='.', types=None):
-        if types is None:
-            src = QtWidgets.QFileDialog.getExistingDirectory(self,
-                                                             '打开目录',
-                                                             default_dir,
-                                                             QtWidgets.QFileDialog.ShowDirsOnly)
-        else:
-            type_str = f'*.{types}'
-            if isinstance(types, list):
-                type_str = f'*.{types[0]}'
-                for t in types[1:]:
-                    type_str += f' *.{t}'
-            src, _ = QtWidgets.QFileDialog.getOpenFileName(self, '打开文件', default_dir, f'File type ({type_str})')
-
-        return src
 
     def __init__widget(self):
         """
@@ -123,13 +30,11 @@ class EzMainWindow(QtWidgets.QMainWindow):
 
         根据上述格式，我们从大到小分块构成布局
         1. 垂直分两块
-        2. 块1水平分5块
+        2. 第一块水平分五块
         :return:
         """
 
         # 首先是从上往下的两个个大区域
-        self.default_layout_space = 5
-        self.default_layout_margins = 2
         self.title_area = self.add_layout_widget(self.central_widget, QtWidgets.QWidget(), space=1)
         self.pages_area = self.add_layout_widget(self.central_widget, QtWidgets.QWidget(), stretch=1)
 
@@ -179,6 +84,6 @@ class EzMainWindow(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = EzMainWindow()
+    window = MainWindow(title='浏览器', default_layout_margins=2, default_layout_space=2, max_window=True)
     window.show()
     sys.exit(app.exec_())
